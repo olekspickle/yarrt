4c5a9a6bd42940bb7aef06c6e0df2c628a5ac00a
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-22 14:11:06 +0100

    surface normals and multiple objects

--- src/lib.rs
+++ src/lib.rs
@@ -34,28 +34,116 @@ impl Ray {
     pub fn point_at(&self, t: f32) -> Vec3 {
         self.a + t * self.b
     }
+}
 
-    pub fn color(&self) -> Vec3 {
-        let t = self.hit_sphere(Vec3::new(-0.0, 0.0, -1.0), 0.5);
-        if t > 0.0 {
-            let n = (self.point_at(t) - Vec3::new(0.0, 0.0, -1.0)).norm();
-            return 0.5 * Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
-        }   
-        let norm = self.direction().norm();
+#[derive(Default, Clone)]
+pub struct Hit {
+    /// Ray parameter. Real number on a line AB, where
+    /// A - ray origin
+    /// B - ray direction:Surface
+    /// --*(t=-1)---A(t=0)--->B(t=1)---*(t=2)---
+    pub t: f32,
+    /// 3D position on the line
+    pub p: Vec3,
+    pub normal: Vec3,
+}
+
+impl Hit {
+    pub fn update(&mut self, t: f32, ray: &Ray, origin: Vec3) {
+        self.set_t(t);
+        self.set_p(ray.point_at(self.t));
+        self.set_normal(self.p - origin);
+    }
+    pub fn set_t(&mut self, t: f32) {
+        self.t = t;
+    }
+    pub fn set_p(&mut self, p: Vec3) {
+        self.p = p;
+    }
+    pub fn set_normal(&mut self, normal: Vec3) {
+        self.normal = normal;
+    }
+}
+
+pub trait Surface {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool;
+}
+
+pub struct Sphere {
+    center: Vec3,
+    radius: f32,
+}
+
+impl Sphere {
+    pub fn new(center: Vec3, radius: f32) -> Self {
+        Sphere { center, radius }
+    }
+    pub fn new_unit() -> Self {
+        Sphere {
+            center: Vec3::ONE,
+            radius: 5.0,
+        }
+    }
+}
+
+impl Surface for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool {
+        let oc: Vec3 = ray.origin() - self.center;
+        let a = ray.direction().dot(ray.direction());
+        let b = oc.dot(ray.direction());
+        let c = oc.dot(oc) - self.radius * self.radius;
+        let discriminant = b * b - a * c;
+        if discriminant > 0.0 {
+            let temp = (-b - discriminant.sqrt()) / a;
+            if temp < t_max && temp > t_min {
+                hit.update(temp, ray, self.center);
+                return true;
+            }
+            let temp = (-b + discriminant.sqrt()) / a;
+            if temp < t_max && temp > t_min {
+                hit.update(temp, ray, self.center);
+                return true;
+            }
+        }
+        false
+    }
+}
+
+pub struct List<T: Surface> {
+    pub list: Vec<T>,
+}
+
+impl<T: Surface> List<T> {
+    pub fn new(list: Vec<T>) -> Self {
+        Self { list }
+    }
+}
+
+impl<T: Surface> Surface for List<T> {
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool {
+        let mut temp_hit = Hit::default();
+        let mut hit_any = false;
+        let mut closest_so_far = t_max;
+        for surface in self.list.iter() {
+            let t = temp_hit.t;
+            if surface.hit(ray, t_min, closest_so_far, &mut temp_hit) {
+                hit_any = true;
+                closest_so_far = t;
+                *hit = temp_hit.clone();
+            }
+        }
+
+        hit_any
+    }
+}
+
+pub fn color<T: Surface>(ray: &Ray, world: &List<T>) -> Vec3 {
+    let mut hit = Hit::default();
+    if world.hit(ray, 0.0, f32::MAX, &mut hit) {
+        return 0.5 * Vec3::new(hit.normal.x + 1.0, hit.normal.y + 1.0, hit.normal.z + 1.0);
+    } else {
+        let norm = ray.direction().norm();
         let t = 0.5 * norm.y + 1.0;
         (1.0 - t) * Vec3::ONE + t * Vec3::new(0.5, 0.7, 1.0)
-    }
-
-    /// We can read this as "any point p that satisfies this equation is on the sphere"
-    /// t*t*dot(B,B) + 2*t*dot(B, A-C) + dot(A-C, A-C) - R*R = 0
-    pub fn hit_sphere(&self, center: Vec3, radius: f32) -> f32 {
-        let oc: Vec3 = self.origin() - center;
-        let a = self.direction().dot(self.direction());
-        let b = 2.0 * oc.dot(self.direction());
-        let c = oc.dot(oc) - radius * radius;
-        let discriminant = b * b - 4.0 * a * c;
-        (discriminant < 0.0)
-            .then(|| -1.0)
-            .unwrap_or_else(|| (-b - discriminant.sqrt()) / (2.0 * a))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@
 
 use glam::Vec3;
 use image::{ImageBuffer, Rgb};
-use rt_in_1_weekend::{utils, Ray};
+use rt_in_1_weekend::{color, utils, List, Ray, Sphere, Surface};
 use std::path::Path;
 
 const DIM: u32 = 200;
-
 
 fn main() {
     let (w, h) = (DIM as f32 * 2.0, DIM as f32);
@@ -22,10 +21,14 @@ fn main() {
         Vec3::new(0.0, -2.0, 0.0),
         Vec3::ZERO,
     );
+    let world = List::new(vec![
+        Sphere::new(Vec3::new(-0.2, 0.0, -1.0), 0.5),
+        Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0),
+    ]);
     for (x, y, pixel) in buf.enumerate_pixels_mut() {
         let (u, v) = ((x as f32 / w), (y as f32 / h));
         let ray = Ray::new(origin, lower_left + u * horiz + v * vert);
-        let col = ray.color();
+        let col = color(&ray, &world);
         *pixel = Rgb([
             (col[0] * 255.99) as u8,
             (col[1] * 255.99) as u8,
